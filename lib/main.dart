20e1db56dcdc1c4a18d6b6f48eb3dae8326f96e7
Ikpambese <ikpambeseb@gmail.com>
2019-11-19 15:43:11 +0100

    added a function to inddicate chosen answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hell0!!'),);
+    var questions = [
+      'What\'s your favorite color',
+      'What\'s your favorite animal '
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MY First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Questions!!'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
